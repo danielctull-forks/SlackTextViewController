94ca24ad69071bfa736ed99310501622decf56c0
Daniel Tull <dt@danieltull.co.uk>
2014-11-14 12:08:23 +0000

    Add a project with a framework target

---
+++ Framework Files/SlackTextViewController.h
@@ -0,0 +1,15 @@
+//
+//  SlackTextViewController.h
+//  SlackTextViewController
+//
+//  Created by Daniel Tull on 14.11.2014.
+//  Copyright (c) 2014 Daniel Tull. All rights reserved.
+//
+
+@import UIKit;
+
+//! Project version number and string for SlackTextViewController.
+FOUNDATION_EXPORT double SlackTextViewControllerVersionNumber;
+FOUNDATION_EXPORT const unsigned char SlackTextViewControllerVersionString[];
+
+#import <SlackTextViewController/SLKTextViewController.h>
